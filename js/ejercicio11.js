850c5f5581c4b4f27d4c1a87d39338a0b639c73c
Jhonatan Poblete <jpobletehernandez@gmail.com>
2026-09-16 20:30:21 -0600

    Add files via upload

---
+++ js/ejercicio11.js
@@ -0,0 +1,24 @@
+function conversion(){
+    var km = document.getElementById("kilometros").value;
+
+    if (km.trim() == ""){
+        alert("Rellene el campo para seguir");
+        return;
+    }
+    
+    if (isNaN(km)){
+        alert("La entrada no es válida");
+        return
+    }
+    
+    var milla=0.621371;
+    
+    var resultado = km*milla;
+
+    document.getElementById("millas").value = resultado.toFixed(2);
+}
+
+document.querySelector(".formulario").addEventListener("submit", function(evento){
+    evento.preventDefault();
+    conversion();
+});
